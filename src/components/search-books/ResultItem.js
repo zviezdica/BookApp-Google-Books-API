32eacdfb9ec300cd572c5bc9d792cc9da7003090
zviezdica <marevic.antonia@gmail.com>
2022-03-23 18:50:19 +0100

    search books background change

--- src/components/search-books/ResultItem.js
+++ src/components/search-books/ResultItem.js
@@ -1,19 +1,29 @@
 import { useState } from "react";
 
-const ResultItem = ({item, handleDetails}) =>{
-    const {id, volumeInfo } = item;
-    const {authors, imageLinks, publishedDate, subtitle, title} = volumeInfo;
-    return(
-        
-            <div className="book-card" onClick={()=>handleDetails(id)}>
-                {title && <h1 className="text-17 text-center pb-10">{title}</h1>}
-                {subtitle && <h3 className="text-14 text-center pb-5">{subtitle}</h3>}
-                {authors && <h4 className="text-14 text-center pb-5">{authors.toString().replace(',', ', ')}, {publishedDate && publishedDate.slice(0,4)}</h4>}
-                {imageLinks && <img className="rounded-sm w-100 my-20" src={imageLinks.smallThumbnail}></img>}
-                <div className="see-more mb-10 capitalize py-2 px-15 md:px-15 text-peacock-blue text-17 transition-all opacity-0">see more</div>
-            </div>
-            
-    )
-}
+const ResultItem = ({ item, handleDetails }) => {
+  const { id, volumeInfo } = item;
+  const { authors, imageLinks, publishedDate, subtitle, title } = volumeInfo;
+  return (
+    <div className="book-card" onClick={() => handleDetails(id)}>
+      {title && <h1 className="text-17 text-center pb-10">{title}</h1>}
+      {subtitle && <h3 className="text-14 text-center pb-5">{subtitle}</h3>}
+      {authors && (
+        <h4 className="text-14 text-center pb-5">
+          {authors.toString().replace(",", ", ")},{" "}
+          {publishedDate && publishedDate.slice(0, 4)}
+        </h4>
+      )}
+      {imageLinks && (
+        <img
+          className="rounded-sm w-100 my-20"
+          src={imageLinks.smallThumbnail}
+        ></img>
+      )}
+      <div className="see-more mb-10 capitalize py-2 px-15 md:px-15 text-peacock-blue text-17 transition-all opacity-0">
+        see more
+      </div>
+    </div>
+  );
+};
 
 export default ResultItem;
